03cb41de86b80b7f4e88bdd7f79d543edec01b9b
katak <katakanakley.27@gmail.com>
2017-04-19 22:27:44 +0300

    event-thumbnail component created

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,11 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'events-list',
-    template: `<div>
-        <h1>Upcoming Angular2 events</h1>
-    </div>`
+    templateUrl: 'app/events/events-list.component.html'
 })
 
 export class EventsListComponent {
+    event1 = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '9/26/2036',
+        time: '10:00 am',
+        price: 599.99,
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        location: {
+            address: '1067 DT',
+            city: 'London',
+            country: 'England'
+        }
+    }
 
+    handleEventClicked(data){
+        console.log('received:', data)
+    }
 }
